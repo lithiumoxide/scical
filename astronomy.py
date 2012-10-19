a11c06bae79e676aa612db9623bb6352edff3caf
Conor Farrell <conor.farrell8@gmail.com>
2012-10-19 23:08:06 +0100

    More

--- astronomy.py
+++ astronomy.py
@@ -11,25 +11,25 @@ def gforce(m1, m2, r):
 	'''
 	
 	global G
-	return (G*m1*m2)/(r**2) + ' N'
+	return str((G*m1*m2)/(r**2)) + ' N'
 	
 def magabs(mapp, d):
 	''' (number, number) -> float
 	Return absolute magnitude given apparent magnitude and distance (parsecs), mapp and d.
 	'''
-	return 5 + mapp - (5*math.log(d))
+	return str(5 + mapp - (5*math.log(d)))
 
 def magapp(mabs, d):
 	''' (number, number) -> float
 	Return apparent magnitude given absolute magnitude and distance (parsecs), mapp and d.
 	'''
-	return (5*math.log(d) - 5) + M
+	return str((5*math.log(d) - 5) + M)
 	
 def luminosity(flux):
 	''' (number) -> float
 	Return luminosity of a star at a given distance d, considering its flux.
 	'''
-	return 4*math.pi*(d**2)*flux
+	return str(4*math.pi*(d**2)*flux) + ' W'
 	
 def schwradius(m):
 	''' (number) -> float
@@ -37,12 +37,12 @@ def schwradius(m):
 	'''
 	global G
 	global c
-	return (2*G*m)/(c**2) + ' m'
+	return str((2*G*m)/(c**2)) + ' m'
 	
 def hubblevel(d):
 	global H
-	return H*d + ' km/s/Mpc'
+	return str(H*d) + ' km/s/Mpc'
 
 def hubbledis(v):
 	global H
-	return v/H + ' km/s'
+	return str(v/H) + ' km/s'
